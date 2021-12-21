3c34c347de6c4371318d45331ca8ad98a8789c38
D. Dessy <ddessy@splunk.com>
2021-12-21 17:31:13 +0100

    Adding MQTT  test

--- test.sh
+++ test.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 source .env
+# Test HEC with token
 curl -k https://localhost:8088/services/collector/event -H "Authorization: Splunk ${SPLUNK_HEC_TOKEN}" -d '{"event": "hello world"}'
-#
-for i in {1..50}; do echo $i;echo "foo:1|c" | nc -u -w0 127.0.0.1 8125; done
+
+# Test MQTT input
+TEMP=$((1 + $RANDOM % 30))
+
+docker container exec mosquitto mosquitto_pub \ 
+  -t 'bedroom/temperature' \
+  -m "bedroom_temperature celsius=$TEMP"
+
+# Test statsd input
+# for i in {1..50}; do echo $i;echo "foo:1|c" | nc -u -w0 127.0.0.1 8125; done
